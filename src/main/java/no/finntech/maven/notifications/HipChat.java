b960cf1a32606862596f06dc3103da23a7bdf7e4
mck <mick@semb.wever.org>
2013-06-29 23:56:35 +0200

    - send a file as a message to a group in yammer - send a message to a list of rooms in hipcat

---
+++ src/main/java/no/finntech/maven/notifications/HipChat.java
@@ -0,0 +1,64 @@
+/** © Copyright 2013 FINN AS
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package no.finntech.maven.notifications;
+
+import com.github.hipchat.api.UserId;
+import com.github.hipchat.api.messages.Message;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+
+/**
+ * Goal which sends notifications to hipchat.
+ */
+@Mojo(name = "hipchat", threadSafe = true)
+public final class HipChat extends AbstractMojo {
+
+    /** comma-separated list of rooms to send notifications to. */
+    @Parameter(property = "hipchat.rooms", required = false)
+    private String hipchatRooms;
+
+    /**
+     * hipchat message to send.
+     */
+    @Parameter(property = "hipchat.message", required = true)
+    private String hipchatMessage;
+
+    /**
+     * The HipChat API token
+     * See https://www.hipchat.com/docs/api
+     */
+    @Parameter(property = "hipchat.token", required = true)
+    private String hipchatToken;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+
+        if(null == hipchatToken || hipchatToken.isEmpty()) {
+            throw new MojoExecutionException("\nhipchatToken isn't defined.");
+        }
+        if(null == hipchatMessage || hipchatMessage.isEmpty()) {
+            throw new MojoExecutionException("\nhipchatMessage isn't defined.");
+        }
+
+        com.github.hipchat.api.HipChat chat = new com.github.hipchat.api.HipChat(hipchatToken);
+        for(String room : hipchatRooms.split(",")) {
+            chat.getRoom(room).sendMessage(hipchatMessage, UserId.create("Maven Release", "Maven Release"), true, Message.Color.PURPLE);
+        }
+    }
+}
---
+++ src/main/java/no/finntech/maven/notifications/Yammer.java
@@ -0,0 +1,93 @@
+/** © Copyright 2013 FINN AS
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package no.finntech.maven.notifications;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+import no.finntech.yammer.YammerClient;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+
+/**
+ * Goal which sends notifications to yammer.
+ */
+@Mojo(name = "yammer", threadSafe = true)
+public final class Yammer extends AbstractMojo {
+
+    @Parameter(defaultValue = "", property = "yammer.groupId", required = false)
+    private String yammerGroupId;
+
+    /**
+     * maven-changes-plugin announcement file.
+     */
+    @Parameter(
+            defaultValue = "${project.build.directory}/announcement/announcement.vm",
+            property = "yammer.announcementFile",
+            required = true)
+    private File yammerAnnouncement;
+
+    /**
+     * The key of the application registered with Yammer. See
+     * http://www.yammer.com/client_applications/new
+     */
+    @Parameter(property = "yammer.applicationKey", required = true)
+    private String yammerApplicationKey;
+
+    /**
+     * The secret of the application registered with Yammer. See
+     * http://www.yammer.com/client_applications/new
+     */
+    @Parameter(property = "yammer.applicationSecret", required = true)
+    private String yammerApplicationSecret;
+
+    /**
+     * project artifactId
+     */
+    @Parameter(defaultValue = "${project.artifactId}", property = "yammer.artifactId", required = false)
+    private String artifactId;
+
+    /**
+     * The Yammer access token, related to the user, used in getting the access authentication.
+     * See http://www.yammer.com/api_oauth.html
+     */
+    @Parameter(property = "yammer.accessToken", required = true)
+    private String yammerAccessToken;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        
+        if(!yammerAnnouncement.exists()) {
+            throw new MojoExecutionException('\n' + yammerAnnouncement.getAbsolutePath() + " doesn't exist.");
+        }
+
+        try (YammerClient client = new YammerClient(yammerApplicationKey, yammerApplicationSecret, yammerAccessToken)) {
+            getLog().info("Posting announcement to yammer");
+            client.sendMessage(
+                    yammerGroupId,
+                    new String(Files.readAllBytes(yammerAnnouncement.toPath()), Charset.forName("UTF-8")),
+                    "release",
+                    artifactId);
+        } catch (IOException ex) {
+            throw new MojoExecutionException("failed… ", ex);
+        }
+    }
+}
